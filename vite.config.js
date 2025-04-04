7166c3b056c85b469f112f85e63a44661722d2ed
Welemhret Baraki <weldat.baraki@gmail.com>
2025-04-04 21:20:56 +0200

    Changing to Fully Html and Java Script from react

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    base: '/portfolio/', // replace with '/' if deploying to your-username.github.io
-    plugins: [react()]
-})
